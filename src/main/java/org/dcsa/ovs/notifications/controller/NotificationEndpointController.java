eba521734151c55e81e8641166c1acf02f07d6b4
Martin Petersson <mp@asseco.dk>
2021-08-16 14:26:53 +0200

    Overwrite CRUD operations with exceptions

--- src/main/java/org/dcsa/ovs/notifications/controller/NotificationEndpointController.java
+++ src/main/java/org/dcsa/ovs/notifications/controller/NotificationEndpointController.java
@@ -7,9 +7,13 @@ import org.dcsa.ovs.notifications.service.NotificationEndpointService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import javax.validation.Valid;
 import java.util.UUID;
 
 @RestController
@@ -34,5 +38,35 @@ public class NotificationEndpointController extends ExtendedBaseController<Notif
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public Mono<Void> receivePayload(ServerHttpRequest request, @PathVariable("id") UUID endpointID) {
         return notificationEndpointService.receiveNotification(request, endpointID);
+    }
+
+    @Override
+    @PostMapping
+    public Mono<NotificationEndpoint> create(@Valid @RequestBody NotificationEndpoint t) {
+        return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN));
+    }
+
+    @Override
+    @GetMapping
+    public Flux<NotificationEndpoint> findAll(ServerHttpResponse response, ServerHttpRequest request) {
+        return Flux.error(new ResponseStatusException(HttpStatus.FORBIDDEN));
+    }
+
+    @Override
+    @DeleteMapping
+    public Mono<Void> delete(@RequestBody NotificationEndpoint t) {
+        return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN));
+    }
+
+    @Override
+    @DeleteMapping({"{id}"})
+    public Mono<Void> deleteById(@PathVariable UUID id) {
+        return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN));
+    }
+
+    @Override
+    @PutMapping({"{id}"})
+    public Mono<NotificationEndpoint> update(@PathVariable UUID id, @Valid @RequestBody NotificationEndpoint t) {
+        return Mono.error(new ResponseStatusException(HttpStatus.FORBIDDEN));
     }
 }
