5ea79189c6180f1c3253883e00958c61dd5f19e6
Martin Petersson <mp@asseco.dk>
2021-08-16 13:05:47 +0200

    POM file updated

--- src/main/java/org/dcsa/ovs/notifications/Application.java
+++ src/main/java/org/dcsa/ovs/notifications/Application.java
@@ -1,9 +1,14 @@
 package org.dcsa.ovs.notifications;
 
+import org.dcsa.core.repository.ExtendedRepositoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
 @SpringBootApplication
+@ComponentScan("org.dcsa")
+@EnableR2dbcRepositories(basePackages = {"org.dcsa"}, repositoryBaseClass = ExtendedRepositoryImpl.class)
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
