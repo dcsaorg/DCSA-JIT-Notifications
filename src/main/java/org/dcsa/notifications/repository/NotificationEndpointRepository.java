a5cd212afded2d6f6e97211ee8b706e57b2d39b5
Martin Petersson <mp@asseco.dk>
2021-08-16 11:52:37 +0200

    remove classes that intellij left behind after refactor

--- src/main/java/org/dcsa/notifications/repository/NotificationEndpointRepository.java
+++
@@ -1,9 +0,0 @@
-package org.dcsa.notifications.repository;
-
-import org.dcsa.core.repository.ExtendedRepository;
-import org.dcsa.notifications.model.NotificationEndpoint;
-
-import java.util.UUID;
-
-public interface NotificationEndpointRepository extends ExtendedRepository<NotificationEndpoint, UUID> {
-}
