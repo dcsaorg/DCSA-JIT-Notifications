85cef4f366ea0021cfefff5832f1414ee24f6e79
Niels Thykier <nt@asseco.dk>
2021-09-09 16:27:02 +0200

    Bump DCSA-Event-Core to 0.0.20
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- src/main/java/org/dcsa/ovs/notifications/service/impl/OVSGenericEventServiceImpl.java
+++ src/main/java/org/dcsa/ovs/notifications/service/impl/OVSGenericEventServiceImpl.java
@@ -1,13 +1,21 @@
 package org.dcsa.ovs.notifications.service.impl;
 
 import org.dcsa.core.events.repository.EventRepository;
+import org.dcsa.core.events.repository.PendingEventRepository;
 import org.dcsa.core.events.service.*;
 import org.dcsa.core.events.service.impl.GenericEventServiceImpl;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OVSGenericEventServiceImpl extends GenericEventServiceImpl implements GenericEventService {
-    public OVSGenericEventServiceImpl(ShipmentEventService shipmentEventService, TransportEventService transportEventService, EquipmentEventService equipmentEventService, OperationsEventService operationsEventService, EventRepository eventRepository) {
-        super(shipmentEventService, transportEventService, equipmentEventService, operationsEventService, eventRepository);
+    public OVSGenericEventServiceImpl(
+            ShipmentEventService shipmentEventService,
+            TransportEventService transportEventService,
+            EquipmentEventService equipmentEventService,
+            OperationsEventService operationsEventService,
+            EventRepository eventRepository,
+            PendingEventRepository pendingEventRepository
+    ) {
+        super(shipmentEventService, transportEventService, equipmentEventService, operationsEventService, eventRepository, pendingEventRepository);
     }
 }
